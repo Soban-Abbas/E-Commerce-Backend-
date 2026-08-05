abe40a9c68a98a13be6a1319b4053f700b744152
Soban Abbas <sobanabbass@gmail.com>
2026-08-05 15:04:53 +0500

    added query for inventoryTable

---
+++ src/models/inventory.model.js
@@ -0,0 +1,26 @@
+const { pool } = require("../config/pool");
+
+
+
+
+
+async function createInventoryTable() {
+    try {
+        await pool.query(`
+           
+
+CREATE TABLE inventory(
+    id SERIAL PRIMARY KEY,
+    product_id INT UNIQUE REFERENCES products(id) ON DELETE CASCADE,
+    quantity INT NOT NULL DEFAULT 0 CHECK(quantity >= 0),
+    updated_at TIMESTAMP DEFAULT now()
+);
+            `)
+    } catch (error) {
+        throw error
+    }
+}
+
+
+module.exports = createInventoryTable
+
--- src/models/product_categories.model.js
+++ src/models/product_categories.model.js
@@ -3,9 +3,11 @@ const {pool}=require("../config/pool");
 async function createproduct_CategoriesTable() {
     try {
         await pool.query(`
+             create table if not exist product_categories (
               product_id INT REFERENCES products(id) ON DELETE CASCADE,
     category_id INT REFERENCES categories(id) ON DELETE CASCADE,
     PRIMARY KEY (product_id, category_id)
+             )
             `)
     } catch (error) {
         throw error
