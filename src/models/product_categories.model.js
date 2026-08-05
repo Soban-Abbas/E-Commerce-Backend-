a9c9ac836c84d8da42c10f38c3e7aae96b87f865
Soban Abbas <sobanabbass@gmail.com>
2026-08-05 13:29:54 +0500

    addded table query for product_categories table

---
+++ src/models/product_categories.model.js
@@ -0,0 +1,16 @@
+const {pool}=require("../config/pool");
+
+async function createproduct_CategoriesTable() {
+    try {
+        await pool.query(`
+              product_id INT REFERENCES products(id) ON DELETE CASCADE,
+    category_id INT REFERENCES categories(id) ON DELETE CASCADE,
+    PRIMARY KEY (product_id, category_id)
+            `)
+    } catch (error) {
+        throw error
+    }
+}
+
+
+module.exports = createproduct_CategoriesTable
